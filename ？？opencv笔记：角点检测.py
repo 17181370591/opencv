51bd2cfa3cba7e5da3b815c7d1ff835114f4329a
17181370591 <34680371+17181370591@users.noreply.github.com>
2018-07-12 11:26:13 +0800

    Update ？？opencv笔记：角点检测.py

--- ？？opencv笔记：角点检测.py
+++ ？？opencv笔记：角点检测.py
@@ -90,7 +90,7 @@ corners = np.int0(corners)
 
 for i in corners:
     x,y = i.ravel()
-    cv2.circle(img,(x,y),3,255,-1)
+    cv2.circle(img,(x,y),3,(0,0,255),-1)
 
 cv2.imwrite('a3.jpg',img)
 plt.imshow(img)
