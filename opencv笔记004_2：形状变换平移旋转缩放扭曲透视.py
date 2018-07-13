bb331b1381b11a57bef2a99b8e999406349405c2
17181370591 <34680371+17181370591@users.noreply.github.com>
2018-07-13 22:12:36 +0800

    Update and rename 3.py to opencv笔记004_2：形状变换平移旋转缩放扭曲透视.py

--- opencv笔记004_2：形状变换平移旋转缩放扭曲透视.py
+++ opencv笔记004_2：形状变换平移旋转缩放扭曲透视.py
@@ -1,60 +1,82 @@
+#仿射变换类似于css的倾斜skew，一个矩形变换完后是个平行四边形
+#透视常用于将四边形转换成矩形，类似于将照片拉伸裁剪成证件照的操作
+#仿射变换后平行四边形的各边仍操持平行，透视变换结果允许是梯形等四边形，所以仿射变换是透视变换的子集
+
 import numpy as np
 import cv2,time
 from matplotlib import pyplot as plt
 
-def nn(x):pass
+def nn(x):
+  pass
 im1=cv2.imread('2.jpg')
 
-#resize
+
+
+#resize改变尺寸，一种是传入变换后的尺寸，一种是传入变换后的比值
 im2=cv2.resize(im1,(500,500))
 im3=cv2.resize(im1,None,fx=.5,fy=.6)
 
-#warpAffine平移
+
+
+
 '''
 图像的平移
 
-下面介绍的图像操作假设你已经知道了为什么需要用矩阵构造才能实现了（上面那个博客有介绍为什么）。那么关于偏移很简单，图像的平移，沿着x方向tx距离，y方向ty距离，那么需要构造移动矩阵： 
+下面介绍的图像操作假设你已经知道了为什么需要用矩阵构造才能实现了（上面那个博客有介绍为什么）。
+那么关于偏移很简单，图像的平移，沿着x方向tx距离，y方向ty距离，那么需要构造移动矩阵： 
 M=[1001txty]
 M=[10tx01ty]
 
 通过numpy来产生这个矩阵，并将其赋值给仿射函数cv2.warpAffine(). 
-仿射函数cv2.warpAffine()接受三个参数，需要变换的原始图像，移动矩阵M 以及变换的图像大小（这个大小如果不和原始图像大小相同，那么函数会自 动通过插值来调整像素间的关系）。
+仿射函数cv2.warpAffine()接受三个参数，需要变换的原始图像，
+移动矩阵M 以及变换的图像大小（这个大小如果不和原始图像大小相同，那么函数会自 动通过插值来调整像素间的关系）。
 '''
-H = np.float32([[1,0,100],[0,1,50]])
+
+H = np.float32([[1,0,100],[0,1,50]])                          #平移矩阵
 rows,cols = im1.shape[:2]
-res = cv2.warpAffine(im1,H,(cols,rows)) #需要图像、变换矩阵、变换后的大小
+res = cv2.warpAffine(im1,H,(cols,rows))                       #需要图像、变换矩阵、变换后的大小
 plt.subplot(121)
 plt.imshow(im1)
 plt.subplot(122)
 plt.imshow(res)
 #plt.show()
 
+
+
+
 #图像的旋转+平移+缩放
 #下面的变形是以原图中点为中心，逆时针旋转180度并将长宽减半
 #用getRotationMatrix2D求出变形矩阵M
 #求出M后，warpAffine里依次传入原图，矩阵M和变形后的画布尺寸，就能得到新图片
+
 rows,cols = im1.shape[:2]
+
 # 这里的第一个参数为旋转中心，第二个为旋转角度，第三个为旋转后的缩放因子
 # 可以通过设置旋转中心，缩放因子，以及窗口大小来防止旋转后超出边界的问题
-M=cv2.getRotationMatrix2D((cols/2,rows/2),180,.5)
+M=cv2.getRotationMatrix2D((cols/2,rows/2),180,.5)                     #旋转缩放矩阵
 print(type(M),M)
+
 # 第三个参数是输出图像的尺寸中心
-dst=cv2.warpAffine(im1,M,(2*cols,2*rows))
+dst=cv2.warpAffine(im1,M,(2*cols,2*rows))                   #没有进行平移，但图片变小了看起来似乎平移了
 cv2.imwrite('dst.jpg',dst)
 #cv2.imshow('img',dst)
 
+
+
+
+
 '''
 仿射变换
-在仿射变换中，原图中所有的平行线在结果图像中同样平行。为了创建这
-个矩阵我们需要从原图像中找到三个点以及他们在输出图像中的位置。然后
-cv2.getAffineTransform 会创建一个 2x3 的矩阵，最后这个矩阵会被传给
-函数 cv2.warpAffine。
+在仿射变换中，原图中所有的平行线在结果图像中同样平行。
+为了创建这个矩阵我们需要从原图像中找到三个点以及他们在输出图像中的位置。
+然后cv2.getAffineTransform 会创建一个 2x3 的矩阵，最后这个矩阵会被传给函数 cv2.warpAffine。
 来看看下面的例子，以及我选择的点（被标记为绿色的点）
 '''
+
 #获取三个点在平移前和平移后的坐标，用getAffineTransform计算平移矩阵M
 pts1=np.float32([[50,50],[200,50],[50,200]])
-pts2=np.float32([[10,100],[200,50],[100,250]])
-M=cv2.getAffineTransform(pts1,pts2)
+pts2=np.float32([[10,100],[200,50],[100,250]])                    #三个点被扭曲（仿射变换）后的位置
+M=cv2.getAffineTransform(pts1,pts2)                               #获取仿射变换的矩阵
 dst=cv2.warpAffine(im1,M,(cols,rows))
 plt.figure(12)
 plt.subplot(121)
@@ -62,6 +84,8 @@ plt.imshow(im1)
 plt.subplot(122)
 plt.imshow(dst)
 #plt.show()
+
+
 
 '''
 透视变换
