98c5fb127b0942f6abd408ffda8e481899c0d60c
17181370591 <34680371+17181370591@users.noreply.github.com>
2018-07-14 15:19:08 +0800

    Update and rename 3.py to opencv笔记005_2：图像梯度.py

--- opencv笔记005_2：图像梯度.py
+++ opencv笔记005_2：图像梯度.py
@@ -1,8 +1,33 @@
 # -*- coding: utf-8 -*-
+
+
 """
-Created on Sun Jan 12 11:01:40 2014
-@author: duan
+图像梯度
+目标
+• 图像梯度，图像边界等
+• 使用到的函数有：cv2.Sobel()，cv2.Schar()，cv2.Laplacian() 等
+原理
+梯度简单来说就是求导。
+OpenCV 提供了三种不同的梯度滤波器，或者说高通滤波器：Sobel，Scharr 和 Laplacian。我们会意义介绍他们。
+Sobel，Scharr 其实就是求一阶或二阶导数。Scharr 是对 Sobel（使用小的卷积核求解求解梯度角度时）的优化。
+Laplacian 是求二阶导数。
+
+18.1 Sobel  算子和 Scharr  算子
+Sobel 算子是高斯平滑与微分操作的结合体，所以它的抗噪声能力很好。
+你可以设定求导的方向（xorder 或 yorder）。还可以设定使用的卷积核的大小（ksize）。
+如果 ksize=-1，会使用 3x3 的 Scharr 滤波器，它的的效果要比 3x3 的 Sobel 滤波器好（而且速度相同，
+所以在使用 3x3 滤波器时应该尽量使用 Scharr 滤波器）。3x3 的 Scharr 滤波器卷积核如下：
+x 方向
+-3 0 3
+-10 0 10
+-3 0 3
+y 方向
+-3 -10 -3
+0 0 0
+3 10 3
 """
+
+
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
