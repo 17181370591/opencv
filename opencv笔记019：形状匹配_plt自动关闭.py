8e38b3ca703323385845aae9a7c9f1a8d0f38047
17181370591 <34680371+17181370591@users.noreply.github.com>
2018-07-15 12:10:22 +0800

    Update and rename 3.py to opencv笔记019：形状匹配_plt自动关闭.py

--- opencv笔记019：形状匹配_plt自动关闭.py
+++ opencv笔记019：形状匹配_plt自动关闭.py
@@ -1,4 +1,11 @@
-import cv2,time
+'''
+形状匹配
+函数 cv2.matchShape() 可以帮我们比较两个形状或轮廓的相似度。如果返回值越小，匹配越好。
+它是根据 Hu  矩来计算的。似乎图片的尺寸和旋转对匹配值几乎没有影响。
+下面的代码使用了随机尺寸+循环旋转进行测试
+'''
+
+import cv2,time,random
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -22,9 +29,9 @@ a=(z.shape[1],z.shape[0])
 b=(np.int(z.shape[1]/2),np.int(z.shape[0]/2))
 i=30
 while 1:
-    M=cv2.getRotationMatrix2D(b,i,1)
+    ran=random.random()*2
+    M=cv2.getRotationMatrix2D(b,i,ran)
     p=cv2.warpAffine(z,M,a)
-    #p=np.zeros_like(img1)
     plt.subplot(1,2,1)
     plt.imshow(z)
     plt.subplot(1,2,2)
@@ -35,4 +42,6 @@ while 1:
     plt.pause(1.3)
     plt.close()
     i+=30
+    if i>333:
+        break
 
