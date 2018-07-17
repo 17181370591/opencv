426bae343515bb7724cb439bbf74644ca2b8ec1f
17181370591 <34680371+17181370591@users.noreply.github.com>
2018-07-17 22:09:38 +0800

    Update and rename 3.py to ？？opencv笔记：人脸识别CascadeClassifier.py

--- ？？opencv笔记：人脸识别CascadeClassifier.py
+++ ？？opencv笔记：人脸识别CascadeClassifier.py
@@ -1,14 +1,11 @@
 '''
 OpenCV  中的 Haar  级联检测
-OpenCV 自带了训练器和检测器。如果你想自己训练一个分类器来检测
-汽车，飞机等的话，可以使用 OpenCV 构建。其中的细节在这里：Cascade
-Classifier Training
-现在我们来学习一下如何使用检测器。OpenCV 已经包含了很多已经训练
-好的分类器，其中包括：面部，眼睛，微笑等。这些 XML 文件保存在/opencv/
-data/haarcascades/文件夹中。下面我们将使用 OpenCV 创建一个面部和眼
-部检测器。
-首先我们要加载需要的 XML 分类器。然后以灰度格式加载输入图像或者
-是视频。
+OpenCV 自带了训练器和检测器。如果你想自己训练一个分类器来检测汽车，飞机等的话，可以使用 OpenCV 构建。
+
+现在我们来学习一下如何使用检测器。OpenCV 已经包含了很多已经训练好的分类器，其中包括：面部，眼睛，微笑等。
+这些 XML 文件保存在/opencv/data/haarcascades/文件夹中。下面我们将使用 OpenCV 创建一个面部和眼部检测器。
+首先我们要加载需要的 XML 分类器。然后以灰度格式加载输入图像或者是视频。xml文件上传在jpg
+
 '''
 import numpy as np
 import cv2
