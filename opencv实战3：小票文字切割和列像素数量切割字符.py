39135245a12efe05d04ad52cc2e229b2b12bf14c
17181370591 <34680371+17181370591@users.noreply.github.com>
2018-07-23 15:40:14 +0800

    Update opencv实战3：小票文字切割和列像素数量切割字符.py

--- opencv实战3：小票文字切割和列像素数量切割字符.py
+++ opencv实战3：小票文字切割和列像素数量切割字符.py
@@ -7,9 +7,13 @@
 
 import matplotlib.pyplot as plt,cv2,numpy as np,pytesseract,PIL
 
-a=cv2.imread('小票.jpg',1)
-b=cv2.imread('小票.jpg',0)
+#注意这里cv2似乎不认识汉字
+pic='小票.jpg'
+a=cv2.imread(pic,1)
+aa=a.copy()
+b=cv2.imread(pic,0)
 ret, b = cv2.threshold(b,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
 
 kernel = np.ones((1,15),np.uint8)        
 #b= cv2.dilate(b,kernel,iterations = 1)                  #对方用了这个，我发现这个其实不需要
@@ -18,9 +22,10 @@ b=cv2.morphologyEx(b, cv2.MORPH_CLOSE, kernel)
 i,contours,h= cv2.findContours(b,0,cv2.CHAIN_APPROX_NONE)
 
 #下面生成的img是贴合每个字符的轮廓
-#img= cv2.drawContours(a, contours, -1, (0,255,0),2)        
+aa= cv2.drawContours(aa, contours, -1, (0,255,0),2)        
 for cnt in contours:                                              #生成每块字符的轮廓矩形
     x,y,w,h = cv2.boundingRect(cnt)
     a = cv2.rectangle(a,(x,y),(x+w,y+h),(0,255,0),2)
-
-cv2.imshow('',a)
+    
+cv2.imshow('aa',aa)
+cv2.imshow('a',a)
